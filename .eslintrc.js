a0bd88cb5517f8c365f8eaa0480d95f20507f178
Erik Perri <erik.perri@gmail.com>
2024-06-06 19:54:15 -0400

    build: switched to type checked linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,22 @@ module.exports = {
   extends: [
     '@react-native',
     'eslint:recommended',
-    'plugin:@typescript-eslint/strict',
+    'plugin:@typescript-eslint/strict-type-checked',
   ],
   ignorePatterns: ['node_modules', 'android', 'ios'],
+  overrides: [
+    {
+      files: ['*.js'],
+      extends: ['plugin:@typescript-eslint/disable-type-checked'],
+    },
+  ],
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  rules: {
+    'no-void': 0,
+  },
 };
