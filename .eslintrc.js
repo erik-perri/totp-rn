39f2df4df4b7c6e29a7738bf8f821a607ddb8325
Erik Perri <erik.perri@gmail.com>
2024-06-06 18:39:13 -0400

    build: expanded eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    '@react-native',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/strict',
+  ],
+  ignorePatterns: ['node_modules', 'android', 'ios'],
 };
--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,4 @@
+/* eslint @typescript-eslint/no-var-requires: "off" */
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
 /**
