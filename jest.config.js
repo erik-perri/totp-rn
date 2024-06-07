5a6ade54794d2b3b9f4735d4dbf12045f31f1f1f
Erik Perri <erik.perri@gmail.com>
2024-06-06 22:44:40 -0400

    test: added async storage mock and fixed jest transforms

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,12 @@
+const ignoredPackages = [
+  '@fortawesome/react-native-fontawesome',
+  '@react-native',
+  'react-native',
+];
+
 module.exports = {
   preset: 'react-native',
+  transformIgnorePatterns: [
+    'node_modules/(?!' + ignoredPackages.join('|') + ')',
+  ],
 };
