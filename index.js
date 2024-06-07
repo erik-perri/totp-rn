f5eff57adc3ff17ac7b7ea3dabd8c9417b186eda
Erik Perri <erik.perri@gmail.com>
2024-06-06 22:39:50 -0400

    update: added quick crypto

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
+import {install} from 'react-native-quick-crypto';
+
+install();
 
 AppRegistry.registerComponent(appName, () => App);
